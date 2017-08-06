71a42182d2dd13dee49c6a04f0364e6dc74c7798
Jonathan Freites <jonathanfreites@gmail.com>
2017-08-06 11:31:45 -0500

    Updated nav links on mobile

--- resources/views/site/partials/header.blade.php
+++ resources/views/site/partials/header.blade.php
@@ -36,100 +36,25 @@
                 </a>
                 <!-- menu mobile -->
                 <div class="gdlr-responsive-navigation dl-menuwrapper" id="gdlr-responsive-navigation">
-                    <button class="dl-trigger">Open Menu</button>
+                    <button class="dl-trigger">Abrir Menu</button>
                     <ul id="menu-main-menu" class="dl-menu gdlr-main-mobile-menu">
                         <li id="menu-item-5286" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-4546 current_page_item menu-item-5286"><a href="/">Inicio</a></li>
-                        <li id="menu-item-5283" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5283"><a href="index.html?p=1122">Nuestras marcas</a>
+                        <li id="menu-item-5283" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5283"><a href="/nuestras-marcas">Nuestras marcas</a>
                             <ul class="dl-submenu">
-                                <li id="menu-item-5214" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5214"><a href="index.html?p=1122">Teosyal</a></li>
-                                <li id="menu-item-5312" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5312"><a href="index.html?p=4960">Inno</a></li>
+                                <li id="menu-item-5214" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5214"><a href="#">Teosyal</a></li>
+                                <li id="menu-item-5312" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5312"><a href="#">Inno</a></li>
                             </ul>
                         </li>
-                        <li id="menu-item-5305" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5305"><a href="products/page/1/index.html">Los productos</a>
+                        <li id="menu-item-5305" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5305"><a href="#">Los productos</a>
                             <ul class="dl-submenu">
-                                <li id="menu-item-5310" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5310"><a href="#">Teosyal</a></li>
-                                <li id="menu-item-5309" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5309"><a href="#">Inno</a></li>
+                                <li id="menu-item-5310" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5310"><a href="/catalogo/?name=teosyal">Teosyal</a></li>
+                                <li id="menu-item-5309" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5309"><a href="/catalogo/?name=inno">Inno</a></li>
                             </ul>
                         </li>
-                        <li id="menu-item-5311" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5311"><a href="#">Promociones</a></li>
-                        <li id="menu-item-5169" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5169"><a href="#">Noticias</a>
-                            <!--<ul class="dl-submenu">
-                                <li id="menu-item-5157" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5157"><a href="../skinbeauty.1#">Blog Full</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5201" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5201"><a href="blog-full-with-right-sidebar/page/1/index.html">Blog Full With Right Sidebar</a></li>
-                                        <li id="menu-item-5200" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5200"><a href="blog-full-with-left-sidebar/page/1/index.html">Blog Full With Left Sidebar</a></li>
-                                        <li id="menu-item-5199" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5199"><a href="blog-full-with-both-sidebar/page/1/index.html">Blog Full With Both Sidebar</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5158" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5158"><a href="../skinbeauty.1#">Blog Column</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5192" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5192"><a href="blog-1-column/page/1/index.html">Blog 1 Column (Right Sidebar)</a></li>
-                                        <li id="menu-item-5193" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5193"><a href="blog-2-columns/page/1/index.html">Blog 2 Columns (Right Sidebar)</a></li>
-                                        <li id="menu-item-5195" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5195"><a href="index.html?p=878">Blog 3 Columns</a></li>
-                                        <li id="menu-item-5197" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5197"><a href="blog-4-columns/page/1/index.html">Blog 4 Columns</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5159" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5159"><a href="../skinbeauty.1#">Blog Masonry</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5194" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5194"><a href="blog-2-columns-masonry/page/1/index.html">Blog 2 Columns &#8211; Masonry (Right Sidebar)</a></li>
-                                        <li id="menu-item-5196" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5196"><a href="blog-3-columns-masonry/page/1/index.html">Blog 3 Columns &#8211; Masonry</a></li>
-                                        <li id="menu-item-5198" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5198"><a href="blog-4-columns-masonry/page/1/index.html">Blog 4 Columns &#8211; Masonry</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5166" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5166"><a href="../skinbeauty.1#">Blog Medium</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5278" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5278"><a href="index.html?p=2033">Blog Medium With Right Sidebar</a></li>
-                                        <li id="menu-item-5277" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5277"><a href="index.html?p=2035">Blog Medium With Left Sidebar</a></li>
-                                        <li id="menu-item-5276" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5276"><a href="blog-medium-with-both-sidebar/page/1/index.html">Blog Medium With Both Sidebar</a></li>
-                                        <li id="menu-item-5275" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5275"><a href="blog-medium-without-sidebar/page/1/index.html">Blog Medium Without Sidebar</a></li>
-                                    </ul>
-                                </li>
-                            </ul>-->
+                        <li id="menu-item-5311" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5311"><a href="/noticias?cat=2">Promociones</a></li>
+                        <li id="menu-item-5169" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5169"><a href="/noticias">Noticias</a>
                         </li>
-                        <li id="menu-item-5262" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5262"><a href="contact-page">Contacto</a>
-                            <!--<ul class="dl-submenu">
-                                <li id="menu-item-5160" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5160"><a href="../skinbeauty.1#">Shortcode Set 1</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5171" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5171"><a href="index.html?p=149">Accordion/Toggle Box</a></li>
-                                        <li id="menu-item-5190" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5190"><a href="index.html?p=784">Audio Shortcode</a></li>
-                                        <li id="menu-item-5172" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5172"><a href="index.html?p=156">Button</a></li>
-                                        <li id="menu-item-5212" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5212"><a href="index.html?p=1017">Column Shortcode</a></li>
-                                        <li id="menu-item-5175" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5175"><a href="index.html?p=203">Divider</a></li>
-                                        <li id="menu-item-5186" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5186"><a href="index.html?p=705">Gallery Shortcode</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5161" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5161"><a href="../skinbeauty.1#">Shortcode Set 2</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5183" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5183"><a href="index.html?p=296">Heading Tag</a></li>
-                                        <li id="menu-item-5185" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5185"><a href="index.html?p=653">icons &#038; icon boxes</a></li>
-                                        <li id="menu-item-5188" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5188"><a href="index.html?p=211">Lightbox &#038; Frames</a></li>
-                                        <li id="menu-item-5179" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5179"><a href="index.html?p=236">Notification Boxes</a></li>
-                                        <li id="menu-item-5176" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5176"><a href="index.html?p=456">Personnel</a></li>
-                                        <li id="menu-item-5211" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5211"><a href="index.html?p=1039">Post Slider</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5162" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5162"><a href="../skinbeauty.1#">Shortcode Set 3</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5216" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5216"><a href="index.html?p=1400">Price Table</a></li>
-                                        <li id="menu-item-5213" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5213"><a href="index.html?p=1084">Process</a></li>
-                                        <li id="menu-item-5180" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5180"><a href="index.html?p=258">Progress Circle</a></li>
-                                        <li id="menu-item-5181" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5181"><a href="index.html?p=245">Skills</a></li>
-                                        <li id="menu-item-5210" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5210"><a href="index.html?p=1045">Slider Shortcode</a></li>
-                                        <li id="menu-item-5184" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5184"><a href="index.html?p=640">Space</a></li>
-                                    </ul>
-                                </li>
-                                <li id="menu-item-5163" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-5163"><a href="../skinbeauty.1#">Shortcode Set 4</a>
-                                    <ul class="dl-submenu">
-                                        <li id="menu-item-5173" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5173"><a href="index.html?p=116">Styled Box</a></li>
-                                        <li id="menu-item-5182" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5182"><a href="index.html?p=285">Stunning Text</a></li>
-                                        <li id="menu-item-5187" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5187"><a href="index.html?p=687">Table</a></li>
-                                        <li id="menu-item-5178" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5178"><a href="index.html?p=218">Tabs</a></li>
-                                        <li id="menu-item-5177" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5177"><a href="index.html?p=428">Testimonial &#038; Quote</a></li>
-                                        <li id="menu-item-5174" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5174"><a href="index.html?p=120">Typography</a></li>
-                                        <li id="menu-item-5189" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-5189"><a href="index.html?p=797">Video</a></li>
-                                    </ul>
-                                </li>
-                            </ul>-->
+                        <li id="menu-item-5262" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-5262"><a href="/contacto">Contacto</a>
                         </li>
                     </ul>
                 </div>
